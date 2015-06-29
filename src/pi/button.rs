540285bd11690caeb3a029fc5547aabf01c44a91
Blake Loring <blake_l@parsed.uk>
2015-06-29 17:49:35 +0100

    Update button.rs

--- src/pi/button.rs
+++ src/pi/button.rs
@@ -1,6 +1,6 @@
 use gpio::{Pin, Direction, State};
 
-pub enum ButtonState { Pressed, NotPressed };
+pub enum ButtonState { Pressed, NotPressed }
 
 pub trait Button {
   pub fn read_state(&self) -> ButtonState;
