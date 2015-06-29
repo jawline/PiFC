55fce72150ef1ab52cad9853c98d64e2185d5c88
Blake Loring <blake_l@parsed.uk>
2015-06-29 10:30:41 +0100

    Create polled_button.rs

---
+++ src/pi/polled_button.rs
@@ -0,0 +1,26 @@
+use button::{Button, ButtonState};
+use gpio::{Pin, Direction, State};
+
+pub struct PolledButton {
+  pin : Pin
+}
+
+impl PolledButton {
+  pub fn new(pin : Pin) -> PolledButton {
+    pin.set_mode(Direction::In);
+    PolledButton{pin : pin}
+  }
+}
+
+impl Button for PolledButton {
+  pub fn read_state(&self) -> ButtonState {
+    if let Option(state) = self.pin.read() {
+      match state {
+        State::High => ButtonState::Pressed,
+        State::Low => ButtonState::NotPressed
+      }
+    } else {
+      ButtonState::NotPressed
+    }
+  }
+}
