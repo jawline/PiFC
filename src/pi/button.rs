df90faba93b70512c2a9becc7ff92e46d3cad254
Blake Loring <blake_l@parsed.uk>
2015-06-29 10:25:08 +0100

    Update button.rs

--- src/pi/button.rs
+++ src/pi/button.rs
@@ -2,26 +2,6 @@ use gpio::{Pin, Direction, State};
 
 pub enum ButtonState { Pressed, NotPressed };
 
-pub struct Button {
-  pin : Pin
-}
-
-impl Button {
-
-  pub fn new(pin : Pin) -> Button {
-    Button{ pin : pin };
-    pin.set_mode(Direction::In);
-  }
-  
-  pub fn get_state() -> ButtonState {
-    if let Option(pin_state) {
-      match pin_state {
-        State::High => Pressed,
-        State::Low => NotPressed
-      }
-    } else {
-      NotPressed
-    }
-  }
-
+pub trait Button {
+  pub fn read_state(&self) -> ButtonState;
 }
