e749b1873c01c25419cc5a1b36f6c55ce55316b8
Blake Loring <blake_l@parsed.uk>
2015-06-30 09:43:15 +0100

    Update gpio.rs

--- src/pi/gpio.rs
+++ src/pi/gpio.rs
@@ -63,7 +63,7 @@ impl Pin {
         let read_result = file_open_res.unwrap().read_to_string(&mut direction_str);
         
         match read_result {
-            Ok(res) => match read_result {
+            Ok(res) => match direction_str {
                      "in" => Some(Direction::In),
                      "out" => Some(Direction::Out),
                      _ => None
