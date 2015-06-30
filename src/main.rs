9a6b335873d3fdbae79bb9574f4542c923ba1fc6
Blake Loring <blake_l@parsed.uk>
2015-06-30 10:51:56 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 mod pi;
 
-use pi::gpio::{Pin, Direction, State};
+use pi::gpio::Pin;
+use pi::light::{Light, LightState};
+use pi::polled_button::PolledButton;
+
+const status_light_pin : usize = 5;
+const switch_in_pin : usize = 6;
 
 fn main() {
-	let mut x = Pin::new(4);
-	x.set_mode(Direction::Out);
-	x.write(State::High);
+	let status_light = Light::new(Pin::new(status_light_pin));
+	let switch_in = PolledButton::new(Pin::new(switch_in_pin));
+	
+	loop {
+		status_light.set_state(match switch_in.read_state() {
+			Pressed => On,
+			NotPressed => Off
+		});
+	}
 }
