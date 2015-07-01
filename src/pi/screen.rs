9c6e6d314871fe2ab11ede3532fe4328da2e4d0a
Blake Loring <blake_l@parsed.uk>
2015-07-01 14:00:18 +0100

    Update screen.rs

--- src/pi/screen.rs
+++ src/pi/screen.rs
@@ -1,11 +1,11 @@
 pub trait Screen {
-  fn resolution() -> (usize, usize);
+  fn resolution(&self) -> (usize, usize);
 }
 
 pub trait PixelScreen {
-  fn set(x : usize, y : usize, rgb : (u8, u8, u8));
+  fn set(&self, x : usize, y : usize, rgb : (u8, u8, u8));
 }
 
 pub trait TextScreen {
-  fn set(x : usize, y : usize, text : char);
+  fn set(&self, x : usize, y : usize, text : char);
 }
