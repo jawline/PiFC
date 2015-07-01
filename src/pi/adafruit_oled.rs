8e16572ab56f6af02847fe6760f884839478af91
Blake Loring <blake_l@parsed.uk>
2015-07-01 13:52:05 +0100

    Update adafruit_oled.rs

--- src/pi/adafruit_oled.rs
+++ src/pi/adafruit_oled.rs
@@ -7,7 +7,12 @@ pub struct AdafruitOled {
 }
 
 impl Screen for AdafruitOled {
+  fn resolution() -> (usize, usize) {
+    (128, 64)
+  }
 }
 
 impl PixelScreen for AdafruitOled {
+  fn set(x : usize, y : usize, (r : u8, g : u8, b : u8)) {
+  }
 }
