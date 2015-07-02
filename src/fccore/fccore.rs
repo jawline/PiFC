3a03fce0581be5ce9eef36dc7d2e5e1e017d8a7e
Blake Loring <blake_l@parsed.uk>
2015-07-02 14:23:20 +0100

    Update fccore.rs

--- src/fccore/fccore.rs
+++ src/fccore/fccore.rs
@@ -6,9 +6,9 @@ pub struct FCCore {
   config : FCConfig
 }
 
-impl FCCore {
-  pub fn new(config_file : &str) -> FCCore {
-    
+impl<'a> FCCore<'a> {
+  pub fn new(config_file : &str) -> FCCore<'a> {
+  
     let mut core = FCCore{
       armed: false,
       config: FCConfig::new(config_file)
@@ -18,12 +18,13 @@ impl FCCore {
     return core;
   }
   
-  fn start_thread(&mut self) {
+  fn<T: FnMut() + 'a> start_thread(&mut self) {
     spawn(|| {
       self.fccore_thread_loop();
     });    
   }
   
   fn fccore_thread_loop(&mut self) {
+    self.armed = false;
   }
 }
