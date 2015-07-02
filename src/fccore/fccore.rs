c05cb91ded5a107a0c5b7bf4584872eb047389bf
Blake Loring <blake_l@parsed.uk>
2015-07-02 16:15:56 +0100

    Update fccore.rs

--- src/fccore/fccore.rs
+++ src/fccore/fccore.rs
@@ -1,18 +1,28 @@
 use fccore::fcconfig::FCConfig;
+use pi::gpio::Pin;
+use pi::light::{Light, LightState};
+use pi::button::{Button, ButtonState};
+use pi::polled_button::PolledButton;
 use std::thread::{spawn, JoinHandle, sleep_ms};
 use std::sync::{Arc, Mutex};
 
 pub struct FCCore {
   armed : bool,
+  status_led : Light,
+  arm_switch : Button,
   config : FCConfig
 }
 
 impl FCCore {
   pub fn new(config_file : &str) -> Arc<Mutex<FCCore>> {
+
+    let config = FCConfig::new(config_file);
   
     let core = Arc::new(Mutex::new(FCCore{
       armed: false,
-      config: FCConfig::new(config_file)
+      status_led : Light::new(Pin::new(config.status_pin)),
+      arm_switch : PolledButton::new(Pin::new(config.arm_switch_pin)),
+      config: config
     }));
     
     let thread_core = core.clone();
