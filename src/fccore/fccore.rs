57909c5453bfdedcb526887169a34b13b7fcc012
Blake Loring <blake_l@parsed.uk>
2015-07-02 16:41:22 +0100

    Update fccore.rs

--- src/fccore/fccore.rs
+++ src/fccore/fccore.rs
@@ -6,7 +6,7 @@ use pi::polled_button::PolledButton;
 use std::thread::{spawn, JoinHandle, sleep_ms};
 use std::sync::{Arc, Mutex};
 
-pub struct FCCore {
+struct FCCore {
   pub armed : bool,
   status_led : Light,
   arm_switch : PolledButton,
