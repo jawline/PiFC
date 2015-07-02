feb5b9e707685f06ffd508d54b3c149e61a6f04b
Blake Loring <blake_l@parsed.uk>
2015-07-02 11:13:55 +0100

    Update fcconfig.rs

--- src/fcconfig/fcconfig.rs
+++ src/fcconfig/fcconfig.rs
@@ -1,10 +1,20 @@
+use rustc_serialize::json;
+use std::fs::File;
+use std::io::{Write, Read};
+
 pub struct FCConfig {
   status_pin : usize,
   arm_switch_pin : usize
 }
 
 impl FCConfig {
+  fn read_config_file(base_file : &str) -> String {
+    let mut result : String;
+    File::open(base_file).unwrap().read_to_string(&mut result);
+    return result;
+  }
   pub fn new(base_file : &str) -> FCConfig {
-    return FCConfig{};
+    let text = FCConfig::read_config_file(base_file);
+    return json::decode(&text).unwrap();
   }
 }
