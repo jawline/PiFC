4aefbaacce4687bb577c26ffbc80618628af6669
Blake Loring <blake_l@parsed.uk>
2015-07-03 15:05:24 +0100

    Update fclog.rs

--- src/fccore/fclog.rs
+++ src/fccore/fclog.rs
@@ -1,5 +1,19 @@
 use std::string::ToString;
+use time::Timespec;
 
 struct LogEntry {
-  
+  info : String,
+  time_entered : Timespec
+}
+
+impl LogEntry {
+  pub fn new(info : &str) -> LogEntry {
+    LogEntry{info: info.to_string(), time_entered: time::get_time()}
+  }
+}
+
+impl ToString for LogEntry {
+  fn to_string(&self) -> String {
+    format!("{}: {}", self.time_entered, self.info);
+  }
 }
