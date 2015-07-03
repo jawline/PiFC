2927c8ca8a08b222a263f1a34c3f704ec089c36f
Blake Loring <blake_l@parsed.uk>
2015-07-03 16:40:18 +0100

    Update fclog.rs

--- src/fccore/fclog.rs
+++ src/fccore/fclog.rs
@@ -15,7 +15,7 @@ impl LogEntry {
 
 impl ToString for LogEntry {
   fn to_string(&self) -> String {
-    format!("{}: {}", self.time_entered.rfc822().to_string(), self.info)
+    format!("{}: {}", self.time_entered.rfc822(), self.info)
   }
 }
 
