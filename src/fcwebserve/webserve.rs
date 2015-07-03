9b92a44872bf6f95c9cb8926cf9d87af1e04e970
Blake Loring <blake_l@parsed.uk>
2015-07-03 10:26:23 +0100

    Update webserve.rs

--- src/fcwebserve/webserve.rs
+++ src/fcwebserve/webserve.rs
@@ -1,11 +1,37 @@
 use iron::prelude::*;
 use iron::status;
+use fccore::FCCore;
+use std::sync::{Arc, Mutex};
 
-pub struct WebServe {
+fn landing_page( req : &mut Request ) -> IronResult<Response>  {
+ Ok(Response::with((status::Ok, "Landing Page!!")))
 }
 
-impl WebServe {
-  pub new() -> WebServe {
-    WebServe{}
+fn page_handler(req : &mut Request, core : Arc<Mutex<FCCore>>) -> IronResult<Response> {    	
+  println!("Length: {}", req.url.path.len());
+  
+  if req.url.path.len() == 0 || (req.url.path.len() == 1 && req.url.path[0] == "") {
+  	return landing_page(req);
   }
+  
+  let mut full_path = String::new();	
+  
+  for partial in req.url.path.iter_mut() {
+    println!("Part{}",partial.to_owned());
+    full_path = full_path + &partial;
+  }
+  
+  Ok(Response::with((status::Ok, full_path.to_owned())))
+}
+
+fn spawn(core : &Arc<Mutex<FCCore>>) {
+  let webserve_core = core.cloned();
+  println!("Spawning WebServe thread");
+
+  spawn(|| {
+    println!("Starting webserve");
+    Iron::new(| req : &mut Request | {
+      page_handle(req, webserve_core);
+    }).http("localhost:3000").unwrap();
+  });
 }
