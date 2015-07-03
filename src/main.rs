2201cd830cd3d9a4f38029a60919816a684c0215
Blake Loring <blake_l@parsed.uk>
2015-07-03 09:50:09 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,8 @@ const BASE_CFG_FILE : &'static str = "./assets/base.cfg";
 
 fn main() {
 	let (core, handle) = fccore::spawn_fc(BASE_CFG_FILE);
-	
-	let config = core.lock().unwrap().config();
 
-	if config.fc_webserve_enabled {
+	if core.lock().unwrap().config().fc_webserve_enabled {
 		println!("I would start webserve, if I knew how");
 	} else {
 		println!("WebServe disabled");
