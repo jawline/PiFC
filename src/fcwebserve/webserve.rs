3459f400f3c464310072ac89bff8c1454282d8f7
Blake Loring <blake_l@parsed.uk>
2015-07-03 14:32:32 +0100

    Update webserve.rs

--- src/fcwebserve/webserve.rs
+++ src/fcwebserve/webserve.rs
@@ -12,7 +12,7 @@ fn status_report(core_ref : &Arc<Mutex<FCCore>>) -> IronResult<Response> {
  let core = core_ref.lock().unwrap();
  
  let response = format!("ALIVE: {}<br/>", core.alive());
- response += format!("ARM_SAFETY: {} ARM_COMMAND: {} FULLY ARMED: {}<br/>", core.armed_switch(), core.armed_cmd(), core.armed());
+ response += &format!("ARM_SAFETY: {} ARM_COMMAND: {} FULLY ARMED: {}<br/>", core.armed_switch(), core.armed_cmd(), core.armed());
 
  Ok(Response::with((status::Ok, response)))
 }
