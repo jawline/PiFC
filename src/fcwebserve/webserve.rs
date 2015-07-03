da52b7cb5c7aadd4f639816cd41638899f4b75e0
Blake Loring <blake_l@parsed.uk>
2015-07-03 13:32:43 +0100

    Update webserve.rs

--- src/fcwebserve/webserve.rs
+++ src/fcwebserve/webserve.rs
@@ -4,25 +4,25 @@ use fccore::FCCore;
 use std::thread;
 use std::sync::{Arc, Mutex};
 
-fn landing_page( req : &mut Request ) -> IronResult<Response>  {
- Ok(Response::with((status::Ok, "Landing Page!!")))
+fn unknown() -> IronResult<Response> {
+ Ok(Response::with(status::NotFound))
+}
+
+fn armed_page(req : &mut Request, core : &Arc<Mutex<FCCore>>) -> IronResult<Response> {
+ Ok(Response::with((status::Ok, core.lock().unwrap().armed.to_string())));
 }
 
 fn page_handler(req : &mut Request, core : &Arc<Mutex<FCCore>>) -> IronResult<Response> {    	
   println!("Length: {}", req.url.path.len());
   
-  if req.url.path.len() == 0 || (req.url.path.len() == 1 && req.url.path[0] == "") {
-  	return landing_page(req);
+  if req.url.path.len() == 1 {
+   match req.url.path[i] {
+    "armed" => armed_page(req, core),
+    _ => unknown()
+   }
+  } else {
+   unknown()
   }
-  
-  let mut full_path = String::new();	
-  
-  for partial in req.url.path.iter_mut() {
-    println!("Part{}",partial.to_owned());
-    full_path = full_path + &partial;
-  }
-  
-  Ok(Response::with((status::Ok, full_path.to_owned())))
 }
 
 pub fn spawn(core : &Arc<Mutex<FCCore>>) {
