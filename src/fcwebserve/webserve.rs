c2f751fbebdd4e8eab84be410a6c1fcc552a3200
Blake Loring <blake_l@parsed.uk>
2015-07-03 10:38:10 +0100

    Update webserve.rs

--- src/fcwebserve/webserve.rs
+++ src/fcwebserve/webserve.rs
@@ -31,7 +31,7 @@ pub fn spawn(core : &Arc<Mutex<FCCore>>) {
 
  thread::spawn(move || {
    println!("Starting webserve");
-    Iron::new(|_: &mut Request| {
+    Iron::new(|req: &mut Request| {
      let response = page_handler(req, webserve_core);
         Ok(Response::with((status::Ok, "Hello world!")))
     }).http("localhost:3000").unwrap();
