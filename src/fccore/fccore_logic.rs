b19e0f8bb5560503f049760c4f65885bfd72df3c
Blake Loring <blake_l@parsed.uk>
2015-07-03 14:20:17 +0100

    Update fccore_logic.rs

--- src/fccore/fccore_logic.rs
+++ src/fccore/fccore_logic.rs
@@ -3,10 +3,8 @@ use std::sync::{Arc, Mutex};
 use std::thread::{spawn, sleep_ms, JoinHandle};
 
 pub fn start_logic_thread(core : &Arc<Mutex<FCCore>>) -> JoinHandle<()> {
-	let thread_core = core.clone();
-	return spawn(move || {
-      fccore_thread_loop(thread_core);
-    });
+    let thread_core = core.clone();
+    spawn(move || fccore_thread_loop(thread_core); )
 }
 
 fn fccore_thread_loop(core_ref : Arc<Mutex<FCCore>>) {
