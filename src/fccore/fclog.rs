29ab151a0d826b4c409f4976c9e85c12445bd594
Blake Loring <blake_l@parsed.uk>
2015-07-03 16:15:06 +0100

    Update fclog.rs

--- src/fccore/fclog.rs
+++ src/fccore/fclog.rs
@@ -1,4 +1,5 @@
 use std::string::ToString;
+use std::vec::Vec;
 use time;
 
 struct LogEntry {
@@ -15,5 +16,31 @@ impl LogEntry {
 impl ToString for LogEntry {
   fn to_string(&self) -> String {
     format!("{:?}: {}", self.time_entered, self.info)
+  }
+}
+
+pub struct Log {
+  pub entries : Vec<LogEntry>
+}
+
+impl Log {
+  pub fn new() -> LogEntry {
+    Log{entries:Vec::new()}
+  }
+
+  pub fn add(&mut self, info : &str) {
+    self.entries.push(LogEntry::new(info));
+  }
+}
+
+impl ToString for Log {
+  fn to_string(&self) -> String {
+    let mut log_data = String::new();
+    
+    for item in self.entries {
+      log_data = log_data + &item.to_string();
+    }
+    
+    log_data
   }
 }
