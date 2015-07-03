65fff480ffe781dbf2fb6da14c11fbf88474bf47
Blake Loring <blake_l@parsed.uk>
2015-07-03 09:47:08 +0100

    Update mod.rs

--- src/fccore/mod.rs
+++ src/fccore/mod.rs
@@ -6,7 +6,7 @@ pub use fccore::fccore::FCCore;
 use std::thread::{JoinHandle};
 use std::sync::{Arc, Mutex};
 
-pub fn spawn_core(base_cfg_path : &str) -> (Arc<Mutex<FCCore>>, JoinHandle<()>) {
+pub fn spawn_fc(base_cfg_path : &str) -> (Arc<Mutex<FCCore>>, JoinHandle<()>) {
 	let core = Arc::new(Mutex::new(FCCore::new(base_cfg_path)));
 	let handle = fccore_logic::start_logic_thread(&core);
 	return (core, handle);
