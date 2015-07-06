1c41c3e780aa99bb34d0c25c456f1fd368a06602
Blake Loring <blake_l@parsed.uk>
2015-07-06 13:30:06 +0100

    Update state.rs

--- src/fccore/motors/state.rs
+++ src/fccore/motors/state.rs
@@ -1,7 +1,19 @@
-pub struct State;
+use fccore::motors::Motor;
+
+pub struct State {
+    pub motor_1 : Motor,
+    pub motor_2 : Motor,
+    pub motor_3 : Motor,
+    pub motor_4 : Motor
+}
 
 impl State {
     pub fn new() -> State {
-        State{}
+        State{
+            motor_1: Motor::new(),
+            motor_2: Motor::new(),
+            motor_3: Motor::new(),
+            motor_4: Motor::new()
+        }
     }
 }
