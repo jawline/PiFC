0a58d8b3064cc8ca2a14103d6653e2e98bac53d2
Blake Loring <blake_l@parsed.uk>
2015-07-06 10:06:17 +0100

    Update polled_button.rs

--- src/physical/polled_button.rs
+++ src/physical/polled_button.rs
@@ -4,25 +4,25 @@ use physical::button::{Button, ButtonState};
 use physical::gpio::{Pin, Direction, State};
 
 pub struct PolledButton {
-  pin : Pin
+    pin : Pin
 }
 
 impl PolledButton {
-  pub fn new(pin : Pin) -> PolledButton {
-    pin.set_mode(Direction::In);
-    PolledButton{pin : pin}
-  }
+    pub fn new(pin : Pin) -> PolledButton {
+        pin.set_mode(Direction::In);
+        PolledButton{pin : pin}
+    }
 }
 
 impl Button for PolledButton {
-  fn read_state(&self) -> ButtonState {
-    if let Some(state) = self.pin.read() {
-      match state {
-        State::High => ButtonState::Pressed,
-        State::Low => ButtonState::NotPressed
-      }
-    } else {
-      ButtonState::NotPressed
+    fn read_state(&self) -> ButtonState {
+        if let Some(state) = self.pin.read() {
+            match state {
+                State::High => ButtonState::Pressed,
+                State::Low => ButtonState::NotPressed
+            }
+        } else {
+            ButtonState::NotPressed
+        }
     }
-  }
 }
