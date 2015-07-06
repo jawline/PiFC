8a84ade0588ba9f4390d8df5bd9f99ba6e239a1e
Blake Loring <blake_l@parsed.uk>
2015-07-06 14:48:14 +0100

    Update state.rs

--- src/fccore/motors/state.rs
+++ src/fccore/motors/state.rs
@@ -1,10 +1,11 @@
 use fccore::motors::Motor;
+use fccore::motors::MotorID;
 
 pub struct State {
-    pub motor_1 : Motor,
-    pub motor_2 : Motor,
-    pub motor_3 : Motor,
-    pub motor_4 : Motor
+    motor_1 : Motor,
+    motor_2 : Motor,
+    motor_3 : Motor,
+    motor_4 : Motor
 }
 
 impl State {
@@ -14,6 +15,24 @@ impl State {
             motor_2: Motor::new(),
             motor_3: Motor::new(),
             motor_4: Motor::new()
+        }
+    }
+    
+    pub fn moter(&self, id: MotorID) -> &Motor {
+        match id {
+            Motor1 => &self.motor_1,
+            Motor2 => &self.motor_2,
+            Motor3 => &self.motor_3,
+            Motor4 => &self.motor_4
+        }
+    }
+    
+    pub fn moter_mut(&mut self, id: MotorID) -> &mut Motor {
+        match id {
+            Motor1 => &mut self.motor_1,
+            Motor2 => &mut self.motor_2,
+            Motor3 => &mut self.motor_3,
+            Motor4 => &mut self.motor_4
         }
     }
 }
