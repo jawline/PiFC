fcf4af1d3ec0f70aaa801fc9e554e96a06401b26
Blake Loring <blake_l@parsed.uk>
2015-07-06 10:33:57 +0100

    Update state.rs

--- src/fccore/telemetry/state.rs
+++ src/fccore/telemetry/state.rs
@@ -1,9 +1,17 @@
-pub struct State;
+pub struct State {
+    pub acc : (f64, f64, f64),
+    pub gyro : (f64, f64, f64)
+}
 
 impl State {
     pub fn new() -> State {
-        State{}
+        State{acc: (0, 0, 0), gyr: (0, 0, 0)}
     }
-    
-    pub fn refresh() {}
+
+    pub fn sample(&mut self) {}
+
+    pub fn clear(&mut self) {
+        self.acc = (0, 0, 0);
+        self.gyro = (0, 0, 0);
+    }
 }
