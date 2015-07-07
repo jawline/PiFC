f8e2cc3e25a671318bf2105bb22e97a1e0765531
Blake Loring <blake_l@parsed.uk>
2015-07-07 13:35:58 +0100

    Update mod.rs

--- src/fccore/mod.rs
+++ src/fccore/mod.rs
@@ -15,7 +15,7 @@ const TAG : &'static str = "fccore";
 /**
  * create a core instance and a core logic thread and return a mutex and handle to them
  */
-pub fn spawn_fc(base_cfg_path : &str) -> (Arc<Mutex<FCCore>>, JoinHandle<()>) {
+pub fn spawn_fc(base_cfg_path : &str) -> (Arc<Mutex<Core>>, JoinHandle<()>) {
     let core = Arc::new(Mutex::new(FCCore::new(base_cfg_path)));
     let handle = fccore_logic::start_logic_thread(&core);
     core.lock().unwrap().log_mut().add(TAG, "done spawning core");
