0efd771a4c8bd5261d40be38291e04463f7fb766
Blake Loring <blake_l@parsed.uk>
2015-07-07 13:35:42 +0100

    Update fccore_logic.rs

--- src/fccore/fccore_logic.rs
+++ src/fccore/fccore_logic.rs
@@ -4,13 +4,13 @@ use std::thread::{spawn, sleep_ms, JoinHandle};
 
 const TAG : &'static str = "fccore_logic";
 
-pub fn start_logic_thread(core : &Arc<Mutex<FCCore>>) -> JoinHandle<()> {
+pub fn start_logic_thread(core : &Arc<Mutex<Core>>) -> JoinHandle<()> {
     let thread_core = core.clone();
     thread_core.lock().unwrap().log_mut().add(TAG, "starting logic thread loop");
     spawn(move || { fccore_thread_loop(thread_core); } )
 }
 
-fn fccore_thread_loop(core_ref : Arc<Mutex<FCCore>>) {
+fn fccore_thread_loop(core_ref : Arc<Mutex<Core>>) {
     core_ref.lock().unwrap().log_mut().add(TAG, "started logic thread loop");
 
     while core_ref.lock().unwrap().alive {
