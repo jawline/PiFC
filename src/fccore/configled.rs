945bf8d1661f5b6182a7d4da60214ab0f00934a5
Blake Loring <blake_l@parsed.uk>
2015-07-08 14:07:12 +0100

    Create configled.rs

---
+++ src/fccore/configled.rs
@@ -0,0 +1,26 @@
+use physical::gpio::Pin;
+use physical::light::{Light, LightState};
+use fccore::config::Light;
+
+pub struct ConfigLed {
+	  enabled: bool,
+	  light: Light
+}
+
+impl ConfigLed {
+    pub fn new(led: &Light) -> ConfigLed {
+        ConfigLed{
+            enabled: led.use_switch,
+            light: Light::new(Pin::new(led.pin))
+        }
+    }
+
+  	pub fn set(&self, state: bool) {
+    	if self.enabled {
+      	    self.light.set_state(match state {
+                true => LightState::On,
+                false => LightState::Off
+            });
+        };
+  	}
+}
