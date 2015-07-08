0d4739f14f488af250a20d9fbda75a5e2de70dd1
Blake Loring <blake_l@parsed.uk>
2015-07-08 13:29:37 +0100

    Update webserve.rs

--- src/fcwebserve/webserve.rs
+++ src/fcwebserve/webserve.rs
@@ -65,18 +65,33 @@ fn motor_test(core_ref: &Arc<Mutex<Core>>) -> IronResult<Response> {
     let mut core = core_ref.lock().unwrap();
     
     core.set_motor_power(MotorID::FrontLeft, 25);
+    core.set_motor_power(MotorID::FrontRight, 25);
+    core.set_motor_power(MotorID::BackLeft, 25);
+    core.set_motor_power(MotorID::BackRight, 25);
     thread::sleep_ms(1000);
     
     core.set_motor_power(MotorID::FrontLeft, 50);
+    core.set_motor_power(MotorID::FrontRight, 50);
+    core.set_motor_power(MotorID::BackLeft, 50);
+    core.set_motor_power(MotorID::BackRight, 50);
     thread::sleep_ms(1000);
     
     core.set_motor_power(MotorID::FrontLeft, 75);
+    core.set_motor_power(MotorID::FrontRight, 75);
+    core.set_motor_power(MotorID::BackLeft, 75);
+    core.set_motor_power(MotorID::BackRight, 75);
     thread::sleep_ms(1000);
     
     core.set_motor_power(MotorID::FrontLeft, 100);
+    core.set_motor_power(MotorID::FrontRight, 100);
+    core.set_motor_power(MotorID::BackLeft, 100);
+    core.set_motor_power(MotorID::BackRight, 100);
     thread::sleep_ms(1000);
     
     core.set_motor_power(MotorID::FrontLeft, 0);
+    core.set_motor_power(MotorID::FrontRight, 0);
+    core.set_motor_power(MotorID::BackLeft, 0);
+    core.set_motor_power(MotorID::BackRight, 0);
     thread::sleep_ms(0);
 
     Ok(Response::with((status::Ok, "ok")))
