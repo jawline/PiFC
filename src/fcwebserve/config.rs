4157bb0a77fecd5acf2924d8b77697349a15e886
Blake Loring <blake_l@parsed.uk>
2015-07-21 13:55:06 +0100

    Create config.rs

---
+++ src/fcwebserve/config.rs
@@ -0,0 +1,22 @@
+use rustc_serialize::json;
+use std::fs::File;
+use std::io::{Read};
+use std::string::{String, ToString};
+
+#[derive(RustcEncodable, RustcDecodable)]
+pub struct Config {
+    pub enabled: bool,
+    pub address: String
+}
+
+impl Config {
+    fn read_config_file(base_file : &str) -> String {
+        let mut result = String::new();
+        
+        if let Err(_) = File::open(base_file).unwrap().read_to_string(&mut result) {
+            panic!("Could not read from FCConfig file {}", base_file);
+        }
+    
+        return result;
+    }
+}
