1c3d93403268fb2d0b5592acd7574d2aff61f381
Blake Loring <blake_l@parsed.uk>
2015-07-21 14:04:03 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ mod fccore;
 mod fcwebserve;
 
 const BASE_CFG_FILE : &'static str = "./configs/base.cfg";
+const WEBSERVE_CFG_FILE : &'static str = "./configs/webserve.cfg";
 const TAG : &'static str = "main";
 
 fn main() {
 	let (core, handle) = fccore::spawn_fc(BASE_CFG_FILE);
-	fcwebserve::spawn(&core);
+	fcwebserve::spawn(&core, WEBSERVE_CFG_FILE);
 
 	if handle.join().is_err() {
 		panic!("Error in FCCore thread");
